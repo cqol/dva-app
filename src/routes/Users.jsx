e77d9f186590c32c66df7258cbc07822f75d1ae5
qima.cq <qima.cq@alibaba-inc.com>
2016-12-28 23:49:45 +0800

    add users

--- src/routes/Users.jsx
+++ src/routes/Users.jsx
@@ -13,7 +13,28 @@ import styles from './Users.less';
 function Users() {
 
   const userSearchProps = {};
-  const userListProps = {};
+  const userListProps={
+    total: 3,
+    current: 1,
+    loading: false,
+    dataSource: [
+      {
+        name: '张三',
+        age: 23,
+        address: '成都',
+      },
+      {
+        name: '李四',
+        age: 24,
+        address: '杭州',
+      },
+      {
+        name: '王五',
+        age: 25,
+        address: '上海',
+      },
+    ],
+  };
   const userModalProps = {};
 
   return (
