20ff07bc70d648c44851c0d3d19c09534ef53c70
qima.cq <qima.cq@alibaba-inc.com>
2016-12-29 00:01:16 +0800

    add users

--- src/routes/Users.jsx
+++ src/routes/Users.jsx
@@ -1,4 +1,8 @@
+// ./src/routes/Users.jsx
 import React, { Component, PropTypes } from 'react';
+
+// 引入 connect 工具函数
+import { connect } from 'dva';
 
 // Users 的 Presentational Component
 // 暂时都没实现
@@ -10,32 +14,21 @@ import UserModal from '../components/Users/UserModal';
 // 可以暂时新建一个空的
 import styles from './Users.less';
 
-function Users() {
+function Users({ location, dispatch, users }) {
 
-  const userSearchProps = {};
+  const {
+    loading, list, total, current,
+    currentItem, modalVisible, modalType
+  } = users;
+
+  const userSearchProps={};
   const userListProps={
-    total: 3,
-    current: 1,
-    loading: false,
-    dataSource: [
-      {
-        name: '张三',
-        age: 23,
-        address: '成都',
-      },
-      {
-        name: '李四',
-        age: 24,
-        address: '杭州',
-      },
-      {
-        name: '王五',
-        age: 25,
-        address: '上海',
-      },
-    ],
+    dataSource: list,
+    total,
+    loading,
+    current,
   };
-  const userModalProps = {};
+  const userModalProps={};
 
   return (
     <div className={styles.normal}>
@@ -49,4 +42,14 @@ function Users() {
   );
 }
 
-export default Users;
+Users.propTypes = {
+  users: PropTypes.object,
+};
+
+// 指定订阅数据，这里关联了 users
+function mapStateToProps({ users }) {
+  return {users};
+}
+
+// 建立数据关联关系
+export default connect(mapStateToProps)(Users);
